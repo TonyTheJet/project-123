79bffc9a4e7ea182293a96eb2edf48fdba5aa145
= <tony@tony-anderson.info>
2017-01-12 15:35:29 -0700

    adds more routes

--- routes/web.php
+++ routes/web.php
@@ -11,36 +11,114 @@
 |
 */
 
+/**
+ * home page
+ */
 Route::get('/', function () {
     //return view('welcome');
     return 'tony rocks the world';
 });
 
 
-
+/**
+ * about the company
+ */
 Route::get('/about', function(){
     return 'about our wonderful company';
 });
+
+/**
+ * cart page
+ */
+
+Route::get('/cart', function(){
+    return 'look at my cart, isn\'t it neat?';
+});
+
+
+Route::get('/checkout', function(){
+    return 'yo, check me out';
+});
+
+
+Route::get('/checkout/thanks/{order_id}', function($order_id){
+    return "ORDER ID {$order_id}";
+});
+
+Route::get('/contact', function(){
+    return 'contact us. do it now.';
+});
+
+Route::get('/create-account', function(){
+    return 'create a new account';
+});
+
+Route::get('/get-credits', function(){
+    return 'get yo-self some stuff';
+});
+
+Route::get('/get-credits/thanks/{order_id}', function($order_id){
+    return 'thanks for getting yourself some stuff with order ' . $order_id;
+});
+
+Route::get('/login', function(){
+    return 'log in. now!';
+});
+
+Route::get('/logout', function(){
+    return 'log out. immediately!';
+});
+
+Route::get('/privacy-policy', function(){
+    return 'read this very exciting privacy policy!';
+});
+
+Route::get('/products', function(){
+    return 'look at all of these products!';
+});
+
+Route::get('/products/{filter_data}', function($filter_data){
+    return "filter data {$filter_data}";
+});
+
+Route::get('/products/category/{category_slug}', function($category_slug){
+    return "category {$category_slug}";
+});
+
+Route::get('/products/single/{product_slug}', function($product_slug){
+    return "look at single product {$product_slug}";
+});
+
+Route::get('/products/type/{type_slug}', function($type_slug){
+    return "look at all products with the slug type {$type_slug}";
+});
+
+Route::get('/products/types', function(){
+    return 'info about the various types of products';
+});
+
+Route::get('/seller', function(){
+    return 'seller home';
+});
+
+Route::get('/seller/products/edit/{', function(){
+    return 'look at my cart, isn\'t it neat?';
+});
+
+Route::get('/cart', function(){
+    return 'look at my cart, isn\'t it neat?';
+});
+
+Route::get('/cart', function(){
+    return 'look at my cart, isn\'t it neat?';
+});
+
+Route::get('/cart', function(){
+    return 'look at my cart, isn\'t it neat?';
+});
+
+
 /*
-/about
-/cart
-/checkout
-/checkout/thanks/{ORDER_ID}
-/contact
-/create-account
-/get-credits
-/get-credits/thanks
-/login
-/logout
-/privacy-policy
-/products
-/products?filter={FILTER_DATA}
-/products/category/{CATEGORY_SLUG}
-/products/single/{PRODUCT_SLUG}
-/products/type/{TYPE_SLUG}
-/products/types
-/seller
-/seller/products/edit/{PRODUCT_SLUG}
 /seller/products/submit
 /seller/sales
 /terms-and-conditions
