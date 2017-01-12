55b12060d5b63f91b9d42ab830c5d83b9da72236
= <tony@tony-anderson.info>
2017-01-12 15:06:41 -0700

    adds some shell routes

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,39 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    //return view('welcome');
+    return 'tony rocks the world';
 });
+
+
+
+Route::get('/about', function(){
+    return 'about our wonderful company';
+});
+/*
+/about
+/cart
+/checkout
+/checkout/thanks/{ORDER_ID}
+/contact
+/create-account
+/get-credits
+/get-credits/thanks
+/login
+/logout
+/privacy-policy
+/products
+/products?filter={FILTER_DATA}
+/products/category/{CATEGORY_SLUG}
+/products/single/{PRODUCT_SLUG}
+/products/type/{TYPE_SLUG}
+/products/types
+/seller
+/seller/products/edit/{PRODUCT_SLUG}
+/seller/products/submit
+/seller/sales
+/terms-and-conditions
+/view-orders
+/view-orders/{ORDER_ID}
+
+ */
