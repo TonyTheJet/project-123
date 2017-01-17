931801fdd635624145e9e6abedce0d51bbd2e4cc
= <tony@tony-anderson.info>
2017-01-17 05:17:44 -0700

    Adds blade templates for basic pages

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,6 +1,13 @@
+            <div class="col-md-8 col-md-offset-2" id="footer">
+                <a href="{{ url('/') }}">HOME</a> |
+                <a href="{{ url('/about') }}">ABOUT</a> |
+                <a href="{{ url('/privacy-policy') }}">PRIVACY POLICY</a> |
+                <a href="{{ url('/terms-and-conditions') }}">TERMS & CONDITIONS</a> |
+                <a href="{{ url('/contact') }}">CONTACT US</a>
+            </div>
         </div><!--.row-->
 
-    <!-- Scripts -->
-    <script src="/js/app.js"></script>
+        <!-- Scripts -->
+        <script src="/js/app.js"></script>
     </body>
 </html>
---
+++ resources/views/terms-and-conditions.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('content')
+    <div class="col-md-8 col-md-offset-2">
+        <h1>Terms & Conditions</h1>
+    </div>
+@endsection
