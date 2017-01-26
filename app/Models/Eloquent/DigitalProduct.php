5b267e076dc0d7b27d16fa18266f62545b988a7e
= <tony@tony-anderson.info>
2017-01-25 21:58:42 -0700

    Adds some constants and fillables for DigitalProduct

--- app/Models/Eloquent/DigitalProduct.php
+++ app/Models/Eloquent/DigitalProduct.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class DigitalProduct extends Model
 {
-    //
+    //constants
+    const STATUS_ACTIVE = 'A';
+    const STATUS_DELETED = 'X';
+    const STATUS_DISCONTINUED = 'D';
+    const STATUS_PENDING_APPROVAL = 'P';
+
+
+
+    protected $fillable = [
+        'current_price_credits',
+        'description',
+        'name',
+        'price_credits',
+        'release_date',
+        'status',
+        'type_id'
+    ];
 }
